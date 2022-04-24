4e06848500fb039310c586c2b0cd1dfa4fa728a4
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-04-24 13:17:44 -0300

    11

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react'
  
 export default function App() {
 
-  const carros=['HRV',"Golf",'Focus','Cruze',"Argo"]
-  const listaCarros=carros.map(
-    (c)=>
-    <li>{c</li>
-  )
+  const carros=[
+    {categoria: 'Esporte', preco: '1100000.00', modelo: 'Golf GTI'},
+    {categoria: 'Esporte', preco: '1200000.00', modelo: 'Camaro'},
+    {categoria: 'SUV', preco: '8500000.00', modelo: 'HRV'},
+    {categoria: 'SUV', preco: '6000000.00', modelo: 'T-Cross'},
+    {categoria: 'Ultilitarios', preco: '1500000.00', modelo: 'Ranger'},
+    {categoria: 'Ultilitarios', preco: '1900000.00', modelo: 'Hillux'}
+    ];
+
+  const listaCarros=carros.map( 
+    (c, i)=> 
+  <li key={i}>{i} - {c}</li>
+    )
+
   return (
     <>
-    
-    <li>{listaCarros</li>
-    
+    <li>{listaCarros}</li>
     </>
   );
 }
