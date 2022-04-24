6c21e819efc07b4efd3ad5d412e36e0e699afea7
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-04-24 16:03:24 -0300

    d

--- src/App.js
+++ src/App.js
@@ -5,23 +5,21 @@ export default function App() {
   const [nome,setNome] = useState('')
   const [carro,setCarro] = useState('')
   
-  }
-
   return (
     <>
       <label>Digite Seu Nome </label>
       <input type='text' name='fnome' value={nome}onChange={(e)=>setNome(e.target.value)} />
       <p>Nome Digitado: {nome}</p>
       <label>Selecione um Carro</label>
-      <select value={carro}>
+      <select value={carro} onChange={(e)=>setCarro(e.target.value)}>
         <option value="HRV">HRV</option>
         <option value="golf">golf</option>
         <option value="cruze">cruze</option>
         <option value="argo">argo</option>
-      <select/>
-      <p>Carro: {nome}</p>
+      </select>
+      <p>Carro Selecionado: {carro}</p>
     </>
-  );
+    );
 }
 
 
