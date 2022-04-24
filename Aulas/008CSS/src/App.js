c86b5a83870514d2ea5d665d4e51554849383ffe
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-04-23 22:52:05 -0300

    10

--- Aulas/008CSS/src/App.js
+++ Aulas/008CSS/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Corpo from './components/Corpo'
-import Relogio from './components/Relogio'
 
 
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
 
   return (
     <section className='caixa'>
-      <Relogio/>
       <Header/>
       <Corpo/>
       <a href='https://www.youtube.com' target='blank'>cpb youtube</a>
