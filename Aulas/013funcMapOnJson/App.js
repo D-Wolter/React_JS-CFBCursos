6ee85962704d413c14fba4fa900b082c1c9e86b1
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-04-24 14:18:55 -0300

    g

---
+++ Aulas/013funcMapOnJson/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+ 
+export default function App() {
+
+  const carros=[
+    {categoria: 'Esporte', preco: '1100000.00', modelo: 'Golf GTI'},
+    {categoria: 'Esporte', preco: '1200000.00', modelo: 'Camaro'},
+    {categoria: 'SUV', preco: '8500000.00', modelo: 'HRV'},
+    {categoria: 'SUV', preco: '6000000.00', modelo: 'T-Cross'},
+    {categoria: 'Ultilitarios', preco: '1500000.00', modelo: 'Ranger'},
+    {categoria: 'Ultilitarios', preco: '1900000.00', modelo: 'Hillux'}
+    ];
+
+  const listaCarros=carros.map( 
+    (c, i)=> 
+  <li key={i}>{i} - {c.categoria} - R${c.preco} - {c.modelo}</li>
+    )
+
+  return (
+    <>
+    <li>{listaCarros}</li>
+    </>
+  );
+}
+
+
