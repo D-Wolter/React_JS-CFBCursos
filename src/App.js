48e215993ac327b009105bb51b6575bfbb62db0f
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-04-24 11:14:51 -0300

    11

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react'
-import Led from './components/led'
-
-
-// use state const ligado, setado false para iniciar desligado
+import React,  {useState} from 'react'
+//nesse codigo usamos recurso da renderizaçao condicional para informar o cliente um cumprimento adapatavel a hora local
+//
+//depois cliamos um botao logar que muda o estado de outra mensagem
 export default function App() {
-  const [ligado,setLigado]=useState(false)
+
+  const [log,setLog] =useState(false)
+
+  const msglogin=()=>{
+    return 'Usuario Logado'
+  }
+  const msglogoff=()=>{
+    return 'Favor Logar'
+  }
+
+  const cumprimento = ()=> {
+    const hora=new Date().getHours()
+    if(hora >=0 && hora < 13) {
+      return <p>Bom Dia</p>
+    } else if(hora>=13 && hora < 18){
+      return <p>Boa Tarde</p>
+    }else{
+      return <p>Boa Noite</p>
+    }
+  }
   return (
     <>
-      
-      <Led ligado={ligado} setLigado={setLigado}/>
-      <a 
-      href='https://www.youtube.com'
-      target='blank'
-      >
-        Cfb Cursos</a>
+    {cumprimento()}
+    <p>{log?msglogin():msglogoff()}</p>
+    <button onClick={()=>setLog(!log)}>{log?'Logoff':'Login'}</button>
     </>
-
-    
-  )
+  );
 }
+
+
