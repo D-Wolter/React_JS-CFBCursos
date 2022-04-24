38ef3ce0c09d2575ea699f6bf088e70d2b6c270b
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-04-24 15:22:39 -0300

    44

---
+++ Aulas/014ManipulationElementsOnInput2/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+ 
+export default function App() {
+
+  const [nome,setNome] = useState('')
+  const handleChangeNome=(e)=> {
+    setNome(e.target.value)
+  }
+
+  return (
+    <>
+    <label>Digite Seu Nome </label>
+    <input 
+    type='text' 
+    name='fnome' 
+    value={nome}
+    onChange={(e)=>handleChangeNome(e)} />
+    <p>Nome Digitado: {nome}</p>
+    </>
+  );
+}
+
+
--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useState, useSyncExternalStore } from 'react'
+import React, { useState } from 'react'
  
 export default function App() {
 
   const [nome,setNome] = useState('')
+  const [carro,setCarro] = useState('')
+  
+  }
 
   return (
     <>
-    <label>Digite Seu Nome </label>
-    <input 
-    type='text' 
-    name='fnome' 
-    value={nome}
-    onChange={(e)=>setNome(e.target.value)} />
-    <p>Nome Digitado: {nome}</p>
+      <label>Digite Seu Nome </label>
+      <input type='text' name='fnome' value={nome}onChange={(e)=>setNome(e.target.value)} />
+      <p>Nome Digitado: {nome}</p>
+      <label>Selecione um Carro</label>
+      <select value={carro}>
+        <option value="HRV">HRV</option>
+        <option value="golf">golf</option>
+        <option value="cruze">cruze</option>
+        <option value="argo">argo</option>
+      <select/>
+      <p>Carro: {nome}</p>
     </>
   );
 }
