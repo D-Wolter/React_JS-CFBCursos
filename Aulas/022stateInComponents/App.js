83026912d4f74cfb176d7bfb417c2212bfe55246
Daniel Wolter <88196398+D-Wolter@users.noreply.github.com>
2022-05-03 14:35:08 -0300

    ç

---
+++ Aulas/022stateInComponents/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Carro from './components/Carro'
+
+export default function App() {
+  return (
+    <>
+      <h1> Componentes de Classe</h1>
+      <Carro />
+    </>
+  );
+}
+
+
